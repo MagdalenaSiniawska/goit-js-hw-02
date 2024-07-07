25b928635702482100a67c4a31e7872ae50f81c6
MagdalenaSiniawska <m.paszke94@gmail.com>
2024-07-07 23:46:03 +0200

    Homework tasks

---
+++ js/task-1.js
@@ -0,0 +1,14 @@
+function makeTransaction(quantity, pricePerDroid, customerCredits) {
+    let totalPrice = quantity * pricePerDroid;
+    if (totalPrice <= customerCredits) {
+        console.log(`You ordered ${quantity} droids worth ${totalPrice} credits!`);
+    } else {
+        console.log("Insufficient funds!");
+    }
+}
+
+(makeTransaction(5, 3000, 23000));
+(makeTransaction(3, 1000, 15000));
+(makeTransaction(10, 5000, 8000));
+(makeTransaction(8, 2000, 10000));
+(makeTransaction(10, 500, 5000));
